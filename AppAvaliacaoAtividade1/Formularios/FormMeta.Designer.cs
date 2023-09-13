5bb7d023959ecf78010334b9bac53a11c45ded31
Rahyan Ramos <rahyanramos.dev@gmail.com>
2023-09-12 21:46:16 -0400

    Alteração na tela de Meta

--- AppAvaliacaoAtividade1/Formularios/FormMeta.Designer.cs
+++ AppAvaliacaoAtividade1/Formularios/FormMeta.Designer.cs
@@ -28,12 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbCargo = new System.Windows.Forms.ComboBox();
+            this.tbVendaFunc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbMetaFunc = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(221, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cargo do funcionário";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(242, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Calcular Comissão";
+            // 
+            // cbCargo
+            // 
+            this.cbCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCargo.FormattingEnabled = true;
+            this.cbCargo.Items.AddRange(new object[] {
+            "Vendedor Júnior",
+            "Vendedor Padrão",
+            "Vendedor Master",
+            "Supervisor de Vendas",
+            "Gerente"});
+            this.cbCargo.Location = new System.Drawing.Point(221, 103);
+            this.cbCargo.Name = "cbCargo";
+            this.cbCargo.Size = new System.Drawing.Size(340, 28);
+            this.cbCargo.TabIndex = 3;
+            // 
+            // tbVendaFunc
+            // 
+            this.tbVendaFunc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbVendaFunc.Location = new System.Drawing.Point(221, 188);
+            this.tbVendaFunc.Name = "tbVendaFunc";
+            this.tbVendaFunc.Size = new System.Drawing.Size(340, 26);
+            this.tbVendaFunc.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(221, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Vendas do funcionário";
+            // 
+            // tbMetaFunc
+            // 
+            this.tbMetaFunc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbMetaFunc.Location = new System.Drawing.Point(221, 284);
+            this.tbMetaFunc.Name = "tbMetaFunc";
+            this.tbMetaFunc.Size = new System.Drawing.Size(340, 26);
+            this.tbMetaFunc.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(221, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Meta do funcionário";
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCancelar.Location = new System.Drawing.Point(525, 369);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(109, 40);
+            this.btCancelar.TabIndex = 16;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = false;
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCalcular.Location = new System.Drawing.Point(644, 369);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(109, 40);
+            this.btCalcular.TabIndex = 15;
+            this.btCalcular.Text = "Calcular";
+            this.btCalcular.UseVisualStyleBackColor = false;
+            // 
+            // FormMeta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btCancelar);
+            this.Controls.Add(this.btCalcular);
+            this.Controls.Add(this.tbMetaFunc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbVendaFunc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbCargo);
+            this.Name = "FormMeta";
             this.Text = "FormMeta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbCargo;
+        private System.Windows.Forms.TextBox tbVendaFunc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbMetaFunc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btCancelar;
+        private System.Windows.Forms.Button btCalcular;
     }
 }
